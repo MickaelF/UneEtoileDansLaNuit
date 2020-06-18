4d3fae5eccef58189909319d024aeb80c4564363
Mickaël Fleurus <contact@mickael-fleurus.com>
2020-06-18 23:00:51 +0200

    Add ImGUI to Cmake, and copy ImGUI files to thirdParty folder with script. Move script to dependencies folder.

---
+++ dependencies/dependencies_compilation.bat
@@ -0,0 +1,36 @@
+@echo off
+setlocal EnableDelayedExpansion
+set Compiler="Visual Studio 16 2019"
+set GLFWDir=%~dp0\glfw-3.3.2
+set GLMDir=%~dp0\glm-0.9.9.8
+set GLADDir=%~dp0\glad-0.1.33
+set ImGUI=%~dp0\imgui-1.76
+set BuildDirSuffix=build
+set InstallDir=%~dp0\..\thirdParty
+mkdir %InstallDir%
+
+echo Handling GLFW
+xcopy /S /Y %GLFWDir%\CMake %InstallDir%\glfw3\CMake\
+xcopy /S /Y %GLFWDir%\deps %InstallDir%\glfw3\deps\
+xcopy /S /Y %GLFWDir%\include %InstallDir%\glfw3\include\
+xcopy /S /Y %GLFWDir%\src %InstallDir%\glfw3\src\
+xcopy /Y %GLFWDir%\CMakeLists.txt %InstallDir%\glfw3\
+xcopy /Y %GLFWDir%\cmake_uninstall.cmake.in %InstallDir%\glfw3\
+
+echo Building GLAD
+set BuildDir=%GLADDir%\%BuildDirSuffix%
+mkdir !BuildDir!
+cmake -S %GLADDir% -B "!BuildDir!" -G %Compiler% -DBUILD_SHARED_LIBS=TRUE -DGLAD_API="gl=3.3" -DGLAD_PROFILE=core
+cmake --build !BuildDir! 
+xcopy /S /Y %BuildDir%\include %InstallDir%\glad\
+xcopy /Y %BuildDir%\src\glad.c %InstallDir%\glad\
+
+echo Handling GLM
+xcopy /S /Y %GLMDir%\glm %InstallDir%\glm\
+
+echo Handling ImGUI
+xcopy /Y %ImGUI%\*.h %InstallDir%\imgui\
+xcopy /Y %ImGUI%\*.cpp %InstallDir%\imgui\
+xcopy /S /Y %ImGUI%\misc %InstallDir%\imgui\
+
+pause
